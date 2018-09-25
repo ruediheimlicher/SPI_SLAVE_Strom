f789f42f66fb8ccbf8209a6d457a8240a37626d9
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2018-09-25 22:18:47 +0200

    reaktiviert mit korrektem Pfad (war OG1

---
+++ .dep/SPI_Slave_Strom.o.d
@@ -0,0 +1,80 @@
+SPI_Slave_Strom.o: SPI_Slave_Strom.c \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
+ /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/iom328p.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/delay.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/math.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h \
+ /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/string.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/ctype.h lcd.c lcd.h \
+ /usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h current.c adc.c \
+ adc.h SPI_slave.c
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h:
+
+/usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/stdint.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/iom328p.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/delay.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/math.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h:
+
+/usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/string.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/ctype.h:
+
+lcd.c:
+
+lcd.h:
+
+/usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h:
+
+current.c:
+
+adc.c:
+
+adc.h:
+
+SPI_slave.c:
